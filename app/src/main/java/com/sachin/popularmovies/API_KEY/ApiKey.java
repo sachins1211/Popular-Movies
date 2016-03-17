6bef6a4a83e9e0ee60384493d16b1f21946fa5ac
Sachin Sharma <sachins1211@gmail.com>
2016-03-18 00:47:50 +0530

    API key removed

--- app/src/main/java/com/sachin/popularmovies/API_KEY/ApiKey.java
+++ app/src/main/java/com/sachin/popularmovies/API_KEY/ApiKey.java
@@ -2,5 +2,5 @@ package com.sachin.popularmovies.API_KEY;
 
 
 public class ApiKey {
-    public final static String TMDB_API_KEY = "bc7bb987b24df9d7a6ee6dd0274e7abb"; //ADD YOUR API KEY HERE
+    public final static String TMDB_API_KEY = ""; //ADD YOUR API KEY HERE
 }
